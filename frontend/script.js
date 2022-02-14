08cd852fddbb91b3ee1192765477f33a5a78ad87
NyergesC <cintia.nyerges@gmail.com>
2022-02-14 17:39:19 +0100

    js practice

---
+++ frontend/script.js
@@ -0,0 +1,114 @@
+//console.log("Hello")
+//console.log('Hello')
+//console.log(`backtip`) 
+
+//console.log(typeof 2)
+//console.log(typeof NaN)
+//console.log(0/0)
+//console.log(2.14)
+
+//console.log(true)
+//console.log(false)
+
+//console.log(undefined)
+//console.log(null)
+
+//console.log(typeof undefined)
+//console.log(typeof null)
+
+/*console.log({
+    "title": "Start Coding",
+    "type": "Solo",
+    "difficulty": 4.1,
+    "usefulness": 4.2,
+    "enjoyability": 3.7,
+    "materials": 3.4,
+    "tasks": [
+        {
+            title: "Bank acccounts",
+            description: ""
+        },
+        {
+            title: "100 doors",
+            description: ""
+        },
+        {
+            title: "Word",
+            description:""
+        }
+    ]
+}) */
+
+
+/*var a = "hello1"
+let b = "hello2"
+const c = "hello3"
+
+console.log(a)
+console.log(b)
+console.log(c) */
+
+
+
+/*function d() {
+    if (1+1 === 2) { //csak itt letezik a let es a const
+    }
+    var a = "hello1"
+    let b = "hello2"
+    const c = "hello3"       
+    console.log(a)
+    console.log(b)
+    console.log(c)
+}
+d ()*/
+
+/*let e = 1
+console.log(e)
+e = 2
+console.log(e)
+e = e*2
+console.log(e)
+
+function multiplyByTwo(num) {
+    console.log(num)
+    return num*2
+}
+let f = multiplyByTwo(1)
+console.log(f)
+console.log(multiplyByTwo(1)); */
+
+/*let g = {
+    myMethod: function (){
+        console.log("hello");
+    }
+}
+g.myMethod()*/
+
+/*let window = {
+    addEventListener: function (eventName,eventFunction){
+        if (eventName === "load"){
+            eventFunction()
+        }   
+    }
+}
+window.addEventListener("load",loadEvent) */
+
+let h = {
+    anotherMethod: function (text) {
+        return `You sent this following argument to this method: ${text}`
+    }
+}
+let i = h.anotherMethod("argument")
+console.log(i);
+console.log(h.anotherMethod("another argument"));
+
+
+
+
+
+
+
+
+
+
+
