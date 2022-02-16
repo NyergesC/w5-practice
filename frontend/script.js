c22c1682bc19b6d383c9f7a08d6b5e7a4e5d0813
NyergesC <cintia.nyerges@gmail.com>
2022-02-16 18:02:28 +0100

    changes

--- frontend/script.js
+++ frontend/script.js
@@ -102,13 +102,71 @@ let i = h.anotherMethod("argument")
 console.log(i);
 console.log(h.anotherMethod("another argument"));*/
 
+/*let myString = "Mikkamakka";
 
-function test(num1,num2) {
-    return num1+num2
+let mySecondString = myString;
+
+let myThirdString = "Mikkamakka"
+
+console.log(myString);
+console.log(mySecondString);
+console.log(myString === mySecondString);
+console.log(myString === myThirdString);*/
+
+/*let myObject = {
+    myString: "Mikkamakka"
 }
-let z = test(3,2)
-console.log(z)
-console.log(test(3,4))
+
+let mySecondObject = myObject;
+
+
+let myThirdObject = {
+    myString: "Mikkamakka"
+}
+
+mySecondObject.myString = "domdodom";
+
+let myFourthObject = {...myObject}; //CSAKIS IGY LEHET OBJEKTUMOT MASOLNI
+
+myFourthObject.myString = "Vacskamati";
+
+console.log(myObject.myString); //result domdomdom lesz
+console.log(myFourthObject.myString); //result vacskamati lesz*/
+
+/*console.log(myObject === mySecondObject);
+console.log(myObject === myThirdObject); // false az eredmeny mert a primitivek mashogy viselkednek, eltarolas szinten mashol tarolodnak el az objektumok. */
+
+
+/*console.log("" === "");
+console.log({} === {}); // azert nem igaz mert hiaba ugyanazt a kulcsnevet es erteket adunk hozza de az objektum ujra letrejott teljesen kulon TEHAT 2 UJONNAN LETREHOZOTT NEM TUDUNK OSSZEHASONLITANI EGYENLOVEL*/
+
+//TYPE OF FUNCTION WRITING
+/*window.addEventListener("load", function(){
+    console.log("Betoldott az oldal");    
+})*/
+
+function loadEvent()   {
+    console.log("masodik betoles");
+    let rootElement = document.getElementById("root");
+
+    let card = function (movieRecieved) {
+        return `
+        <div class="card">
+             <h2> ${movieRecieved.title}</h2>
+            <div class="time">${movieRecieved.year}</div>
+            <div class="rate">${movieRecieved.rate}</div>
+        </div>
+        `;
+};
+
+    for (const movieSend of movies) {
+        rootElement.insertAdjacentHTML("beforeend",card(movieSend));
+     }
+     console.log(movies);
+     
+
+}
+window.addEventListener("load", loadEvent)
 
 
 
